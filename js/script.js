36cb7b680bc92a0598c0587868c95a16b1d5f93d
dylannichols <nichodo@g.toiohomai.ac.nz>
2018-06-17 14:13:58 +1200

    Release 1.0.0

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 (function() {
+
+    var submitBtn = document.querySelector(".submit-btn")
+    submitBtn.addEventListener("click", function() {
+        alert("Message Sent!")
+        window.location.href = "dashboard.html"
+    })
+
     if (screen.width > 1024) {
         var addFamily = document.querySelector(".add-family")
         var findWhanau = document.querySelector(".find-whanau")
@@ -8,7 +15,8 @@
         var img1 = document.querySelector(".dash-img1")
         var img2 = document.querySelector(".dash-img2")
         var form = document.querySelector(".dash-form")
-        var messageGrid = document.querySelector(".message-grid")
+        var messageGrid = document.querySelector(".message-grid2")
+        var map = document.querySelector(".map")
         var addFamilyLink = document.querySelectorAll(".link")
 
         for (var i = 0; i < addFamilyLink.length; i++) {
@@ -18,10 +26,11 @@
         }
 
         addFamily.addEventListener("click", function(e) {
+            map.style.display = "none"
+            findWhanau.style.display = "block"
             messageGrid.style.display = "none"
             messageWhanau.style.display = "block"
             addFamily.style.display = "none"
-            secondGrid.style.gridTemplateColumns = "1fr"
             img1.style.display = "none"
             img2.style.display = "none"
             findWhanau.style.gridColumn = "1"
@@ -32,10 +41,11 @@
         })
 
         messageWhanau.addEventListener("click", function(e) {
+            map.style.display = "none"
+            findWhanau.style.display = "block"
             form.style.display = "none"
             addFamily.style.display = "block"
             messageWhanau.style.display = "none"
-            secondGrid.style.gridTemplateColumns = "1fr"
             img1.style.display = "none"
             img2.style.display = "none"
             findWhanau.style.gridColumn = "1"
@@ -45,7 +55,16 @@
             messageGrid.style.gridRow = "1/3"
         })
 
-
+        findWhanau.addEventListener("click", function(e) {
+            messageGrid.style.display = "none"
+            messageWhanau.style.display = "block"
+            form.style.display = "none"
+            addFamily.style.display = "block"
+            findWhanau.style.display = "none"
+            map.style.display = "block"
+            img1.style.display = "none"
+            img2.style.display = "none"
+        })
 
     }
 })();
